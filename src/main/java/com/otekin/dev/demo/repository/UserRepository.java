e47240bf51b393fe89653db40f7438f43e45b8a0
Oğuzhan Tekin <119113636+tekinoguzhan@users.noreply.github.com>
2023-10-07 14:21:17 +0300

    Add files via upload

---
+++ src/main/java/com/otekin/dev/demo/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.otekin.dev.demo.repository;
+
+import com.otekin.dev.demo.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
